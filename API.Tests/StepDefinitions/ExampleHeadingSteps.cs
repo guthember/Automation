bba4307ccc6177a6ff95553e6813a0e10acf59a4
guthember <guthember@gmail.com>
2025-08-08 08:05:11 +0200

    add HtmlAgilityPack nuget package

--- API.Tests/StepDefinitions/ExampleHeadingSteps.cs
+++ API.Tests/StepDefinitions/ExampleHeadingSteps.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Tests.StepDefinitions;
+﻿using HtmlAgilityPack;
+
+namespace API.Tests.StepDefinitions;
 
 [Binding]
 [Scope(Feature = "Example.com oldalon a H1 ellenőrzése")]
@@ -11,22 +13,11 @@ public class ExampleHeadingSteps
     [Then(@"the H1 should be ""(.*)""")]
     public void ThenTheH1ShouldBe(string expected)
     {
-        // A HttpClientSteps betette a teljes HTML-t a "content" kulcsba
         var content = _ctx.Get<string>("content");
-
-        const string open = "<h1>";
-        const string close = "</h1>";
-
-        var i1 = content.IndexOf(open, StringComparison.OrdinalIgnoreCase);
-        Assert.That(i1, Is.GreaterThanOrEqualTo(0), "Nem található <h1> tag.");
-
-        i1 += open.Length;
-        var i2 = content.IndexOf(close, i1, StringComparison.OrdinalIgnoreCase);
-        Assert.That(i2, Is.GreaterThanOrEqualTo(0), "Nem található </h1> tag.");
-
-        var actual = content[i1..i2].Trim();
-        Assert.That(actual,
-            Is.EqualTo(expected),
-            $"A H1-nek '{expected}'-nek kell lennie, de '{actual}' volt.");
+        var doc = new HtmlDocument();
+        doc.LoadHtml(content);
+        var h1 = doc.DocumentNode.SelectSingleNode("//h1");
+        Assert.That(h1, Is.Not.Null, "Nem található <h1> tag.");
+        Assert.That(h1.InnerText.Trim(), Is.EqualTo(expected), $"A H1-nek '{expected}'-nek kell lennie.");
     }
 }
